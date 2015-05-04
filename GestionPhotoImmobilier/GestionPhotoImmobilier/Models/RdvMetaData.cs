b2508f79b449c56c1b8faae02c1761db6502150b
William Cantin <william@hugocantin.info>
2015-05-04 10:07:14 -0400

    MetaDatas update

--- GestionPhotoImmobilier/GestionPhotoImmobilier/Models/RdvMetaData.cs
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/Models/RdvMetaData.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace GestionPhotoImmobilier.Models
 {
-    public class RdvMetaData
+    public partial class RdvMetaData
     {
+        public int RdvId { get; set; }
+        [DisplayName("Est confirmé")]
+        public Nullable<bool> Confirmer { get; set; }
+        public string Client { get; set; }
+        public string Photographe { get; set; }
     }
 }
