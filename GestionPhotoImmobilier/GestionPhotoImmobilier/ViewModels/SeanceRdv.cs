b693c77249c9a91fb867316cd0474c7b918c154d
blocdeglace19952 <1250824@LLBD0603INFO14.laboratoire.collegeem.qc.ca>
2015-05-04 11:01:06 -0400

    SeanceRdv

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/ViewModels/SeanceRdv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.ViewModels
+{
+    public class SeanceRdv
+    {
+        //seance
+        public int SeanceId { get; set; }
+        [DisplayName("Date Séance")]
+        public Nullable<System.DateTime> DateSeance { get; set; }
+        public string Agent { get; set; }
+        [DisplayName("Photographe Séance")]
+        public string Photographe { get; set; }
+        public string Client { get; set; }
+        public string Forfait { get; set; }
+        public string Commentaire { get; set; }
+        public string Statut { get; set; }
+        //rdv
+        [DisplayName("RDV Confirmé")]
+        public Nullable<bool> Confirmer { get; set; }
+        [DisplayName("Photographe RDV")]
+        public string PhotographeRDV { get; set; }
+    }
+}
