26bc0e765a66d4a15c6187e7f1421103c9343a73
Darder006 <joel_x34345@hotmail.com>
2015-05-05 09:52:53 -0400

    Trigger RDV Reporté
    Trigger pour le reportage d'un rendez-vous.

--- Script SQL/Sprint 1/S1_06Triggers.sql
+++ Script SQL/Sprint 1/S1_06Triggers.sql
@@ -32,3 +32,40 @@ BEGIN
 	END
 END
 GO
+
+IF OBJECT_ID('Seance.trg_RDVReporte') IS NOT NULL
+	DROP TRIGGER Seance.trg_RDVReporte
+GO
+
+CREATE TRIGGER Seance.trg_RDVReporte
+ON Seance.Seance
+AFTER UPDATE
+AS
+BEGIN
+	DECLARE @idSeance int
+	DECLARE @dateNouvelle DATETIME
+	DECLARE @dateVieille DATETIME
+
+	SELECT @idSeance = i.SeanceId, @dateNouvelle = i.DateSeance
+	FROM inserted i
+
+	SELECT @dateVieille = d.DateSeance
+	FROM deleted d
+	
+	IF((YEAR(@dateNouvelle) != YEAR(@dateVieille)) OR (MONTH(@dateNouvelle) != MONTH(@dateVieille)) OR (DAY(@dateNouvelle) != DAY(@dateVieille)))
+	BEGIN
+		DECLARE @hourVieille int = DATEPART(HOUR, @dateVieille)
+		DECLARE @minuteVieille int = DATEPART(MINUTE, @dateVieille)
+		DECLARE @hourNouvelle int = DATEPART(HOUR, @dateNouvelle)
+		DECLARE @minuteNouvelle int = DATEPART(MINUTE, @dateNouvelle)
+
+		IF((@hourVieille != @hourNouvelle) OR (@minuteVieille != @minuteNouvelle))
+		BEGIN
+			UPDATE Seance.Seance
+			SET Statut = 'Reportee'
+			WHERE SeanceId = @idSeance
+		END
+	END
+
+END
+GO
