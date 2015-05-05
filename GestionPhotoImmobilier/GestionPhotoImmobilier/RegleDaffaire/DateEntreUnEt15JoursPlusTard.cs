68b4c0d2bce45700202b9aa3e846951b00fb0e4b
blocdeglace19952 <1250824@LLBD0603INFO18.laboratoire.collegeem.qc.ca>
2015-05-05 09:56:47 -0400

    Ajout dossier ReglesDaffaire

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/RegleDaffaire/DateEntreUnEt15JoursPlusTard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using GestionPhotoImmobilier.DAL;
+using GestionPhotoImmobilier.Models;
+
+
+namespace GestionPhotoImmobilier.RegleDaffaire
+{
+    public class DateEntreUnEt15JoursPlusTard
+    {
+       public static ValidationResult Validate(GestionPhotoImmobilier.Models.Seance seance)
+       {
+            return ValidationResult.Success;
+        } 
+
+       
+       
+       
+       
+       
+       }
+}
