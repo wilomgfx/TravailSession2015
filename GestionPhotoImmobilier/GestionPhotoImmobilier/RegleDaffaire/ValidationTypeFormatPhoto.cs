6732cc1d0910419034167d84bc760adc2354bf0f
sDesmecht <0934818@LLBD0603INFO10.laboratoire.collegeem.qc.ca>
2015-05-08 14:14:25 -0400

    Validation pour le type de format des photos

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/RegleDaffaire/ValidationTypeFormatPhoto.cs
@@ -0,0 +1,25 @@
+﻿using GestionPhotoImmobilier.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.RegleDaffaire
+{
+    public class ValidationTypeFormatPhoto
+    {
+        public static ValidationResult Validate(GestionPhotoImmobilier.Models.Photo photo)
+        {
+            UnitOfWork unitofWork = new UnitOfWork();
+
+            // Doit vérifier lors de la création d'une photo que seul les formats .jpg, .jpeg et .png sont accepté
+
+            if (photo.TypeFichier == ".jpg" || photo.TypeFichier == ".jpeg" || photo.TypeFichier == ".png")
+                return ValidationResult.Success;
+
+
+            return new ValidationResult("Le format de la photo entré n'est pas supporté, veillez choisir un des format suivant :  .jpg, .jpeg, .png ");
+        } 
+    }
+}
