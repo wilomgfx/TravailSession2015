6b47b9ec5d597cc269d0b713d3d94cf46f29c62c
Darder006 <joel_x34345@hotmail.com>
2015-05-08 12:27:21 -0400

    ViewModel SommaireSeance

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/ViewModels/SommaireSeance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GestionPhotoImmobilier.Models;
+
+namespace GestionPhotoImmobilier.ViewModels
+{
+    public class SommaireSeance
+    {
+        SeanceRdv SeanceRdv { get; set; }
+        Agent Agent { get; set; }
+        Propriete Propriete { get; set; }
+    }
+}
