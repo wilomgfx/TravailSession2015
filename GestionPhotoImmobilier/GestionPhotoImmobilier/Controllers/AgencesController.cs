14dc60ae1643b7022a45b0c789e09bd4e580915e
sDesmecht <0934818@LLBD0603INFO14.laboratoire.collegeem.qc.ca>
2015-05-12 08:40:59 -0400

    AgenceRepository

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/Controllers/AgencesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GestionPhotoImmobilier.Models;
+
+namespace GestionPhotoImmobilier.Controllers
+{
+    public class AgencesController : Controller
+    {
+        private H15_PROJET_E03Entities db = new H15_PROJET_E03Entities();
+
+        // GET: Agences
+        public ActionResult Index()
+        {
+            return View(db.Agences.ToList());
+        }
+
+        // GET: Agences/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // GET: Agences/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agences/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AgenceId,Nom,Adresse,NumTel")] Agence agence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agences.Add(agence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agence);
+        }
+
+        // GET: Agences/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // POST: Agences/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AgenceId,Nom,Adresse,NumTel")] Agence agence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agence);
+        }
+
+        // GET: Agences/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // POST: Agences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agence agence = db.Agences.Find(id);
+            db.Agences.Remove(agence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
