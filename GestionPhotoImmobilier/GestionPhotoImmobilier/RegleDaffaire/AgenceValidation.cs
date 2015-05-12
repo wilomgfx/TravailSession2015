c5a2ea175489cc270a6d2459ccaa81f7ac48b9b4
sDesmecht <0934818@LLBD0603INFO14.laboratoire.collegeem.qc.ca>
2015-05-12 10:30:18 -0400

    Phone Validation pour une agence

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/RegleDaffaire/AgenceValidation.cs
@@ -0,0 +1,17 @@
+﻿using GestionPhotoImmobilier.RegleDaffaire;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.Models
+{
+    public partial class Agence : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield return phoneValidation.Validate(this);
+        }
+    }
+}
