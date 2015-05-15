a26562b95544660a0b5284002d0d8d5782200939
sDesmecht <0934818@LLBD0603INFO08.laboratoire.collegeem.qc.ca>
2015-05-15 13:59:41 -0400

    Validation Photo
    vérifie que le path de la photo est dans le format demandé (jpg, jpeg ou
    png)

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/RegleDaffaire/photoValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.RegleDaffaire
+{
+    public class photoValidation:ValidationAttribute
+    {
+        //Un attribut personnalisé permettra de valider que le format du fichier est l’un de cela 
+        //jpg, jpeg et png
+        public photoValidation():base()
+        {
+
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string ErrorMessage = "Le format de la photo est érroné, veuillez en choisir un parmi les suivants : jpg, jpeg ou png ";
+            if(value != null && value is string )
+            {
+                string path = (string)value;
+                if(path.Contains(".jpg")|| path.Contains(".jpeg")|| path.Contains(".png"))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
