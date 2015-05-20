17d0610f7106a6997434f4f2724f28766b550ed2
sDesmecht <0934818@LLBD0603INFO13.laboratoire.collegeem.qc.ca>
2015-05-20 09:39:50 -0400

    Update trigger a fixer s2_06

--- Script SQL/Sprint 2/S2_06TriggersSuite.sql
+++ Script SQL/Sprint 2/S2_06TriggersSuite.sql
@@ -2,31 +2,6 @@
 If OBJECT_ID('Proprietes.trg_PhotosPrise') IS NOT NULL DROP TRIGGER Proprietes.trg_PhotosPrise;
 Go
 
-
-CREATE TRIGGER Proprietes.trg_PhotosPrise
-ON Proprietes.Photo
-AFTER INSERT
-AS
-BEGIN
-	DECLARE @SeanceId int
-	DECLARE @ProprieteId as int = (SELECT ProprieteId FROM inserted)
-
-	SET @SeanceId = (SELECT SeanceId FROM Seance.Seance WHERE @ProprieteId = ProprieteId) 
-
-	UPDATE Seance.Seance
-	SET Statut='Réalisée'
-	WHERE SeanceId = @SeanceId;
-
-  END 
-GO
---TESTS
-
-
---quand les photos sont disponibles pour l’agent (statut : livrée)
-If OBJECT_ID('Seance.trg_PhotosLivree') IS NOT NULL DROP TRIGGER Seance.trg_PhotosLivree;
-Go
-
-
 CREATE TRIGGER Seance.trg_PhotosLivree
 ON Seance.Seance
 AFTER UPDATE
@@ -41,7 +16,7 @@ BEGIN
 		IF @photoDisponible = 'true'
 		BEGIN
 			UPDATE Seance.Seance
-			SET Statut='Livrée'
+			SET Statut='réalisé'
 			WHERE SeanceId = @SeanceId;
 		END
 		ELSE
@@ -54,6 +29,80 @@ BEGIN
 	
   END 
 GO
+
+
+
+
+
+
+--CREATE TRIGGER Proprietes.trg_PhotosPrise
+--ON Proprietes.Photo
+--AFTER INSERT
+--AS
+--BEGIN
+--	DECLARE @SeanceId int
+--	DECLARE @ProprieteId as int = (SELECT ProprieteId FROM inserted)
+
+--	SET @SeanceId = (SELECT SeanceId FROM Seance.Seance WHERE @ProprieteId = ProprieteId) 
+
+--	UPDATE Seance.Seance
+--	SET Statut='Réalisée'
+--	WHERE SeanceId = @SeanceId;
+
+--  END 
+--GO
+--TESTS
+
+
+--quand les photos sont disponibles pour l’agent (statut : livrée)
+If OBJECT_ID('Seance.trg_PhotosLivree') IS NOT NULL DROP TRIGGER Seance.trg_PhotosLivree;
+Go
+
+
+CREATE TRIGGER Seance.trg_PhotosLivree
+ON Proprietes.Photo
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @SeanceId int
+	DECLARE @ProprieteId as int = (SELECT ProprieteId FROM inserted)
+
+	SET @SeanceId = (SELECT SeanceId FROM Seance.Seance WHERE @ProprieteId = ProprieteId) 
+
+	UPDATE Seance.Seance
+	SET Statut='Livrée'
+	WHERE SeanceId = @SeanceId;
+
+  END 
+GO
+
+
+--ON Seance.Seance
+--AFTER UPDATE
+--AS
+--BEGIN
+--	DECLARE @SeanceId as int = (Select SeanceId FROM (Select TOP 1 * FROM inserted ORDER BY SeanceId DESC) AS I);
+--	DECLARE @photoDisponible as bit = (SELECT photoDisponible FROM inserted)
+--	DECLARE @oldStatut as nvarchar(50) = (SELECT statut FROM deleted)
+	
+--	IF UPDATE(photoDisponible)
+--	BEGIN
+--		IF @photoDisponible = 'true'
+--		BEGIN
+--			UPDATE Seance.Seance
+--			SET Statut='Livrée'
+--			WHERE SeanceId = @SeanceId;
+--		END
+--		ELSE
+--		BEGIN
+--			UPDATE Seance.Seance
+--			SET Statut = @oldStatut
+--			WHERE SeanceId = @SeanceId
+--		END
+--	END
+	
+--  END 
+--GO
 --TESTS
 --INSERT INTO Seance.Seance(DateSeance,Photographe,Client,Forfait,Commentaire,AgentId,ProprieteId)
 --VALUES('20150508','BoB','Jorge','goodshit',':D',1,1)
