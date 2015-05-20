4b1c2f3ce2ed786fd013a41ef50f054da7c86592
sDesmecht <0934818@LLBD0603INFO13.laboratoire.collegeem.qc.ca>
2015-05-20 10:37:50 -0400

    Validation class champs vide

---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/Models/AgenceMetaData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.Models
+{
+    [MetadataType(typeof(AgenceMetaData))]
+
+    public partial class Agence { }
+    public partial class AgenceMetaData
+    {
+        [Required]
+        public int AgenceId { get; set; }
+         [Required]
+        public string Nom { get; set; }
+         [Required]
+         [MinLength(8)]
+        public string Adresse { get; set; }
+         [Required]
+        public string NumTel { get; set; }
+    }
+}
---
+++ GestionPhotoImmobilier/GestionPhotoImmobilier/Models/AgentMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionPhotoImmobilier.Models
+{
+    [MetadataType(typeof(AgentMetaData))]
+
+    public partial class Agent { }
+    public partial class AgentMetaData
+    {
+        public int AgentId { get; set; }
+        [Required]
+        public string Nom { get; set; }
+    }
+}
